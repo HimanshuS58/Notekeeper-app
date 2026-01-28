d2b371a183369a567ded8b3b5e885f82893abcb1
Himanshu Singh <142159346+HimanshuS58@users.noreply.github.com>
2026-01-28 23:12:44 +0530

    Update url.js

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const BACKEND_URL = axios.create({
-    baseURL: "https://notekeeper-app-t4qw.onrender.com"
+    baseURL: "https://notekeeper-app-t4qw.onrender.com/api/v1/noteapp"
 })
 
 export default BACKEND_URL;
